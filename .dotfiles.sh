0d5153be6d71349b6ca1f59f79403e2398f56c12
Mateusz Adam Katana <mateusz.adam.katana@gmail.com>
2020-08-15 23:24:47 +0200

    Update .dotfiles.sh

--- .dotfiles.sh
+++ .dotfiles.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Configurations
 mkdir -p ~/configuration
 mkdir -p ~/configuration/konsole
 mkdir -p ~/configuration/konsole/tabs
@@ -8,24 +7,19 @@ mkdir -p ~/configuration/ssh
 mkdir -p ~/configuration/ssh/sessions
 mkdir -p ~/configuration/ssh/keys
 
-# Pictures
 mkdir -p ~/pictures
-mkdir -p ~/pictures/avatars
 mkdir -p ~/pictures/wallpapers
 
-# Captures
 mkdir -p ~/captures
 mkdir -p ~/captures/pictures
 mkdir -p ~/captures/videos
 
-# Virtual Machines
 mkdir -p ~/virtual-machines
 mkdir -p ~/virtual-machines/vmware
 mkdir -p ~/virtual-machines/vmware/archives
 mkdir -p ~/virtual-machines/vmware/templates
 mkdir -p ~/virtual-machines/vmware/workspace
 
-# Others
 mkdir -p ~/archives
 mkdir -p ~/bluetooth
 mkdir -p ~/cheat-sheets
@@ -39,7 +33,6 @@ mkdir -p ~/iso-images
 mkdir -p ~/music
 mkdir -p ~/scripts
 mkdir -p ~/portable
-mkdir -p ~/captures
 mkdir -p ~/temporary
 mkdir -p ~/videos
 mkdir -p ~/word-lists
