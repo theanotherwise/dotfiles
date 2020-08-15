9e218e4973d124cb9f5654a1475e5d926073a013
Mateusz Adam Katana <mateusz.adam.katana@gmail.com>
2020-08-15 23:22:36 +0200

    Update .dotfiles.sh

--- .dotfiles.sh
+++ .dotfiles.sh
@@ -39,34 +39,8 @@ mkdir -p ~/iso-images
 mkdir -p ~/music
 mkdir -p ~/scripts
 mkdir -p ~/portable
-mkdir -p ~/projects
 mkdir -p ~/captures
 mkdir -p ~/temporary
 mkdir -p ~/videos
 mkdir -p ~/word-lists
 mkdir -p ~/work
-
-# Projects
-mkdir -p ~/projects/android
-mkdir -p ~/projects/ansible
-mkdir -p ~/projects/bash
-mkdir -p ~/projects/burp
-mkdir -p ~/projects/c
-mkdir -p ~/projects/cpp
-mkdir -p ~/projects/helm
-mkdir -p ~/projects/html
-mkdir -p ~/projects/java
-mkdir -p ~/projects/javscript
-mkdir -p ~/projects/kde-plasma
-mkdir -p ~/projects/node.js
-mkdir -p ~/projects/php
-mkdir -p ~/projects/puppet
-mkdir -p ~/projects/python
-mkdir -p ~/projects/ruby
-mkdir -p ~/projects/soupui
-mkdir -p ~/projects/wsdl
-mkdir -p ~/projects/wsdl
-mkdir -p ~/projects/xca
-mkdir -p ~/projects/zabbix
-mkdir -p ~/projects/terraform
-mkdir -p ~/projects/others
