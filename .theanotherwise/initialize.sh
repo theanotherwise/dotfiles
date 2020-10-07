63922044b834c7f01cd0f26de3d71e9c310d9007
Mateusz Adam Katana <mateusz.adam.katana@gmail.com>
2020-10-07 18:55:13 +0200

    #

--- .theanotherwise/initialize.sh
+++ .theanotherwise/initialize.sh
@@ -16,30 +16,3 @@ mkdir -p ~/scripts
 mkdir -p ~/projects
 mkdir -p ~/git
 mkdir -p ~/temporary
-
-if [ "${1}" = "ws" ] ; then
-  mkdir -p ~/bluetooth
-  mkdir -p ~/captures
-  mkdir -p ~/captures/pictures
-  mkdir -p ~/captures/videos
-  mkdir -p ~/cheat-sheets
-  mkdir -p ~/configuration/konsole
-  mkdir -p ~/configuration/konsole/tabs
-  mkdir -p ~/dashcam
-  mkdir -p ~/desktop
-  mkdir -p ~/diagrams
-  mkdir -p ~/documents
-  mkdir -p ~/ebooks
-  mkdir -p ~/iso-images
-  mkdir -p ~/music
-  mkdir -p ~/pictures
-  mkdir -p ~/pictures/wallpapers
-  mkdir -p ~/videos
-  mkdir -p ~/virtual-machines
-  mkdir -p ~/virtual-machines/vmware
-  mkdir -p ~/virtual-machines/vmware/archives
-  mkdir -p ~/virtual-machines/vmware/templates
-  mkdir -p ~/virtual-machines/vmware/workspace
-  mkdir -p ~/word-lists
-  mkdir -p ~/work
-fi
