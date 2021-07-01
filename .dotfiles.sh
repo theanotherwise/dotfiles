d8eafbd9b453c31285b571f0ce2e1b0b63ff6968
Mateusz Adam Katana <mateusz.adam.katana@gmail.com>
2021-07-01 23:52:03 +0200

    no message

--- .dotfiles.sh
+++ .dotfiles.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 mkdir -p ~/archives
-mkdir -p ~/portable
+
 mkdir -p ~/portable/helm
 mkdir -p ~/portable/ruby
 mkdir -p ~/portable/terraform
 mkdir -p ~/portable/node.js
 mkdir -p ~/portable/yarn
-mkdir -p ~/configurations
-mkdir -p ~/configurations/ssh
+
 mkdir -p ~/configurations/ssh/keys
 mkdir -p ~/configurations/ssh/sessions
+
 mkdir -p ~/downloads
 mkdir -p ~/scripts
 mkdir -p ~/projects
