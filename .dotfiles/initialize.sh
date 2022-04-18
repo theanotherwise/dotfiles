e904c3e0e656f0e99d9e18a187c16feea04ddbe8
Mateusz Adam Katana <mateusz.adam.katana@gmail.com>
2022-04-18 22:49:04 +0200

    Update initialize.sh

--- .dotfiles/initialize.sh
+++ .dotfiles/initialize.sh
@@ -3,18 +3,19 @@
 [ -z "$TMP_HOME" ] && TMP_HOME="${HOME}"
 
 mkdir -p "${TMP_HOME}"/archives
-mkdir -p "${TMP_HOME}"/binaries/helm
-mkdir -p "${TMP_HOME}"/binaries/node
-mkdir -p "${TMP_HOME}"/binaries/ruby
-mkdir -p "${TMP_HOME}"/binaries/kubectl
-mkdir -p "${TMP_HOME}"/binaries/terraform
-mkdir -p "${TMP_HOME}"/binaries/yarn
 mkdir -p "${TMP_HOME}"/downloads
 mkdir -p "${TMP_HOME}"/configs
 mkdir -p "${TMP_HOME}"/sessions
 mkdir -p "${TMP_HOME}"/projects
 mkdir -p "${TMP_HOME}"/scripts/cron.d
 mkdir -p "${TMP_HOME}"/temporary
+
+mkdir -p "${TMP_HOME}"/binaries/helm/bin
+mkdir -p "${TMP_HOME}"/binaries/node/bin
+mkdir -p "${TMP_HOME}"/binaries/ruby/bin
+mkdir -p "${TMP_HOME}"/binaries/kubectl/bin
+mkdir -p "${TMP_HOME}"/binaries/terraform/bin
+mkdir -p "${TMP_HOME}"/binaries/yarn/bin
 
 rm -f "${TMP_HOME}"/README.md "${TMP_HOME}"/.gitignore "${TMP_HOME}"/.dotfiles/initialize.sh
 
