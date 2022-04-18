9ecf8195e605b56bd396c87951d64095f8fab2e5
Mateusz Adam Katana <mateusz.adam.katana@gmail.com>
2022-04-18 23:19:46 +0200

    Update initialize.sh

--- .dotfiles/initialize.sh
+++ .dotfiles/initialize.sh
@@ -5,7 +5,7 @@ function portable() {
   helm)
     URL="https://get.helm.sh/helm-v${2}-linux-amd64.tar.gz"
     ARCHIVE_PATH="${TMP_DIR}/helm.tgz"
-    APP_DIR="${TMP_HOME}/binaries/helm"
+    APP_DIR="${HOME}/binaries/helm"
 
     mkdir -p "${APP_DIR}/${2}/bin"
     ln -s "${APP_DIR}/${2}" "${APP_DIR}/latest"
@@ -16,7 +16,7 @@ function portable() {
   kubectl)
     URL="https://dl.k8s.io/release/v${2}/bin/linux/amd64/kubectl"
     ARCHIVE_PATH="${TMP_DIR}/kubectl"
-    APP_DIR="${TMP_HOME}/binaries/kubectl"
+    APP_DIR="${HOME}/binaries/kubectl"
 
     mkdir -p "${APP_DIR}/${2}/bin"
     ln -s "${APP_DIR}/${2}" "${APP_DIR}/latest"
@@ -27,7 +27,7 @@ function portable() {
   yarn)
     URL="https://github.com/yarnpkg/yarn/releases/download/v${2}/yarn-v${2}.tar.gz"
     ARCHIVE_PATH="${TMP_DIR}/yarn.tgz"
-    APP_DIR="${TMP_HOME}/binaries/yarn"
+    APP_DIR="${HOME}/binaries/yarn"
 
     mkdir -p "${APP_DIR}/${2}/bin"
     ln -s "${APP_DIR}/${2}" "${APP_DIR}/latest"
@@ -38,7 +38,7 @@ function portable() {
   node)
     URL="https://nodejs.org/dist/v${2}/node-v${2}-linux-x64.tar.xz"
     ARCHIVE_PATH="${TMP_DIR}/node.xz"
-    APP_DIR="${TMP_HOME}/binaries/node"
+    APP_DIR="${HOME}/binaries/node"
 
     mkdir -p "${APP_DIR}/${2}/bin"
     ln -s "${APP_DIR}/${2}" "${APP_DIR}/latest"
@@ -49,7 +49,7 @@ function portable() {
   terraform)
     URL="https://releases.hashicorp.com/terraform/${2}/terraform_${2}_linux_amd64.zip"
     ARCHIVE_PATH="${TMP_DIR}/terraform.zip"
-    APP_DIR="${TMP_HOME}/binaries/terraform"
+    APP_DIR="${HOME}/binaries/terraform"
 
     mkdir -p "${APP_DIR}/${2}/bin"
     ln -s "${APP_DIR}/${2}" "${APP_DIR}/latest"
@@ -60,23 +60,22 @@ function portable() {
   esac
 }
 
-[ -z "$TMP_HOME" ] && TMP_HOME="${HOME}"
 TMP_DIR="$(mktemp -p "/tmp" -d XXXXX)"
 
-mkdir -p "${TMP_HOME}"/archives
-mkdir -p "${TMP_HOME}"/downloads
-mkdir -p "${TMP_HOME}"/configs
-mkdir -p "${TMP_HOME}"/sessions
-mkdir -p "${TMP_HOME}"/projects
-mkdir -p "${TMP_HOME}"/scripts/cron.d
-mkdir -p "${TMP_HOME}"/temporary
+mkdir -p "${HOME}"/archives
+mkdir -p "${HOME}"/downloads
+mkdir -p "${HOME}"/configs
+mkdir -p "${HOME}"/sessions
+mkdir -p "${HOME}"/projects
+mkdir -p "${HOME}"/scripts/cron.d
+mkdir -p "${HOME}"/temporary
 
-mkdir -p "${TMP_HOME}"/binaries/helm
-mkdir -p "${TMP_HOME}"/binaries/node
-mkdir -p "${TMP_HOME}"/binaries/ruby
-mkdir -p "${TMP_HOME}"/binaries/kubectl
-mkdir -p "${TMP_HOME}"/binaries/terraform
-mkdir -p "${TMP_HOME}"/binaries/yarn
+mkdir -p "${HOME}"/binaries/helm
+mkdir -p "${HOME}"/binaries/node
+mkdir -p "${HOME}"/binaries/ruby
+mkdir -p "${HOME}"/binaries/kubectl
+mkdir -p "${HOME}"/binaries/terraform
+mkdir -p "${HOME}"/binaries/yarn
 
 portable "helm" "3.8.2"
 portable "kubectl" "1.23.0"
@@ -84,8 +83,8 @@ portable "yarn" "1.22.18"
 portable "node" "16.14.2"
 portable "terraform" "1.1.8"
 
-rm -f "${TMP_HOME}"/README.md
-rm -f "${TMP_HOME}"/.gitignore
-rm -f "${TMP_HOME}"/.dotfiles/initialize.sh
-rm -rf "${TMP_HOME}"/.git
+rm -f "${HOME}"/README.md
+rm -f "${HOME}"/.gitignore
+rm -f "${HOME}"/.dotfiles/initialize.sh
+rm -rf "${HOME}"/.git
 rm -rf "${TMP_DIR}"
