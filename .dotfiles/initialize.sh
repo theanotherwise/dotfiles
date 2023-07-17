fdedbd86566e0652e6b478c3a145f688aa5a2d49
Mateusz Adam Katana <mateusz.adam.katana@gmail.com>
2023-07-17 20:38:06 +0200

    asdf

--- .dotfiles/initialize.sh
+++ .dotfiles/initialize.sh
@@ -272,7 +272,7 @@ function portable() {
       portable_download "${URL}" "${ARCHIVE_PATH}"
       portable_extract_zip "${ARCHIVE_PATH}" "${VER_PATH}"
       echo "${VER_PATH}"
-      mv "${VER_PATH}/groovy-${2}/.*" "${VER_PATH}/../"
+      mv "${VER_PATH}/groovy-${2}/*" "${VER_PATH}/../"
       portable_permissions "${VER_PATH}"
 
       mark_ask_installed "${VER_PATH}"
