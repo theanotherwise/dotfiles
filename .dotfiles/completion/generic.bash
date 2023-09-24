d95c88ae581b877dc0cb799d038d965d722b8b86
Mateusz Adam Katana <mateusz.adam.katana@gmail.com>
2023-09-24 19:40:49 +0200

    asdf

--- .dotfiles/completion/generic.bash
+++
@@ -1,6 +0,0 @@
-complete -o default -F __start_oc311 oc3.11
-complete -o default -F __start_oc410 oc4.10
-complete -o default -F __start_oc411 oc4.11
-complete -o default -F _kube_namespaces kns
-complete -o default -F _kube_contexts kctx
-complete -o default -F __start_kubectl k
