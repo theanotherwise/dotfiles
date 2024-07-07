dd07d84901c04269ba8e3dd2b9c6088ecb9f14c4
Mateusz Adam Katana <mateusz.adam.katana@gmail.com>
2024-07-07 19:07:23 +0200

    dsaf

--- .dotfiles/init.py
+++ .dotfiles/init.py
@@ -10,8 +10,6 @@ import tarfile
 import lzma
 import zipfile
 
-print(os.getcwd())
-
 RANDOM_STRING = ''.join(random.choices(string.ascii_lowercase + string.digits, k=12))
 TMP_PATH = "/tmp/dotfiles-{}".format(RANDOM_STRING)
 
--- .dotfiles/macos.sh
+++ .dotfiles/macos.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+
+script_directory="$(dirname "$(realpath "$0")")"
+
+echo $script_directory
+
 python3 init.py macos
