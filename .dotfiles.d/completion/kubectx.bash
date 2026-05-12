29bd533f64af73ce8ec7c5c11ab9fb759ca0cf71
Mateusz Adam Katana <mateusz.adam.katana@gmail.com>
2026-05-12 10:32:10 +0200

    feat: new features implemented

--- .dotfiles.d/completion/kubectx.bash
+++ .dotfiles.d/completion/kubectx.bash
@@ -1,6 +1,8 @@
-sc_helper_kube_contexts_completion()
+_kube_contexts()
 {
   local curr_arg;
   curr_arg=${COMP_WORDS[COMP_CWORD]}
   COMPREPLY=( $(compgen -W "- $(kubectl config get-contexts --output='name')" -- $curr_arg ) );
 }
+
+complete -F _kube_contexts kubectx kctx
