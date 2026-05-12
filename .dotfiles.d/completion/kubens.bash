f6c2909128c85dabf9b62ba3e91db39a6d3cf8f3
Mateusz Adam Katana <mateusz.adam.katana@gmail.com>
2026-05-12 10:19:18 +0200

    feat: new features implemented

--- .dotfiles.d/completion/kubens.bash
+++ .dotfiles.d/completion/kubens.bash
@@ -1,6 +1,8 @@
 sc_helper_kube_namespaces_completion()
 {
-  local curr_arg;
-  curr_arg=${COMP_WORDS[COMP_CWORD]}
-  COMPREPLY=( $(compgen -W "- $(kubectl get namespaces -o=jsonpath='{range .items[*].metadata.name}{@}{"\n"}{end}')" -- $curr_arg ) );
+  local curr_arg namespaces
+
+  curr_arg="${COMP_WORDS[COMP_CWORD]:-}"
+  namespaces="$(kubectl get namespaces -o jsonpath='{range .items[*]}{.metadata.name}{" "}{end}' 2>/dev/null)"
+  COMPREPLY=( $(compgen -W "- ${namespaces}" -- "${curr_arg}") )
 }
